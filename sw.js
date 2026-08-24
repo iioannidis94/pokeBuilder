0d1a5f52a78c72a406e12ecb9e6f4e6893faa593
Ioannidis Ioannis <109786525+iioannidis94@users.noreply.github.com>
2026-08-24 11:18:29 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -20,6 +20,7 @@ const APP_SHELL = [
   './data/bosses.js',
   './team/team-core.js',
   './team/battle_tower.js',
+  './team/trainer_tower.js',
   './team/team-io.js',
   './team/team-ai.js',
   './team/team-recommender.js',
